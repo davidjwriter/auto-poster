6fe1e145f5ab7cc9e851502185367003afe6da23
spatiumstories <spatium.nfts@gmail.com>
2024-04-11 19:42:33 -0400

    Retrieving substack contents works

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -19,21 +19,21 @@ import { AwsIntegration } from 'aws-cdk-lib/aws-apigateway';
 export class AutoPosterStack extends Stack {
   constructor(app: App, id: string) {
     super(app, id);
-    // dotenv.config({ path: path.resolve(__dirname, '.env') });
     const openAiApiKey = process.env.OPEN_AI_API_KEY || 'NO_API_KEY';
-    const privateKey = process.env.PRIVATE_KEY || 'NO_PRIVATE_KEY';
-    const nftStoreApiKey = process.env.NFT_STORAGE_API_KEY || 'No NFT Store API Key';
-    const cloudConvertApiKey = process.env.CLOUD_CONVERT_API_KEY || 'No Cloud Conver API Key';
+    const twitterKey = process.env.TWITTER_API_KEY || 'NO Twitter API Key';
+    const twitterSecret = process.env.TWITTER_API_SECRET || 'No Twitter Secret';
+    const desoUser = process.env.DESO_USER || "No Deso User";
+    const desoPrivateKey = process.env.DESO_PRIVATE_KEY || "No Deso Private Key";
 
     // Setup our dynamo db table
-    const dynamoTable = new Table(this, 'Recipes', {
+    const dynamoTable = new Table(this, 'Posts', {
       partitionKey: {
         name: 'uuid',
         type: AttributeType.STRING
       },
       readCapacity: 1,
       writeCapacity: 1,
-      tableName: 'Recipes',
+      tableName: 'Posts',
 
       /**
        *  The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
@@ -43,32 +43,6 @@ export class AutoPosterStack extends Stack {
       removalPolicy: RemovalPolicy.RETAIN, // NOT recommended for production code
     });
 
-    // User's dynamo db table
-    const userTable = new Table(this, 'RecipeBooks', {
-      partitionKey: {
-        name: 'username',
-        type: AttributeType.STRING
-      },
-      sortKey: {
-        name: 'uuid',
-        type: AttributeType.STRING
-      },
-      readCapacity: 1,
-      writeCapacity: 1,
-      tableName: 'RecipeBooks'
-    });
-
-
-    // Lambda function to add a new recipe
-    // Expects a string URL
-    const s3Bucket = new s3.Bucket(this, 'RecipeImages', {
-      versioned: true,
-      removalPolicy: RemovalPolicy.RETAIN,
-      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
-      accessControl: BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
-      publicReadAccess: true,
-    });
-
     // Create an IAM role for the Lambda function
     const lambdaRole = new iam.Role(this, 'LambdaRole', {
       assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
@@ -76,158 +50,110 @@ export class AutoPosterStack extends Stack {
 
     // Attach the basic Lambda execution policy (You can adjust permissions as needed)
     lambdaRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'));
-
-    // Create a policy statement for creating SNS topics
-    const sqsPolicyStatement = new iam.PolicyStatement();
-    sqsPolicyStatement.addActions('sqs:CreateQueue', 'sqs:SendMessage'); // Add additional permissions as needed
-    sqsPolicyStatement.addAllResources(); // Grant access to all SNS resources, adjust as needed
-
-    // Attach the policy statement to the IAM role
-    lambdaRole.addToPolicy(sqsPolicyStatement);
   
+    // Create an SNS topic and subscribe the postToTwitter and postToDeso lambdas
+    const postTopic = new sns.Topic(this, 'NewPostTopic');
 
-    const addRecipeWorker = new Function(this, 'addRecipeWorker', {
-      description: "Add recipes worker",
-      code: Code.fromAsset('lib/lambdas/addRecipeWorker/target/x86_64-unknown-linux-musl/release/lambda'),
+    // Event triggered Lambdas: generatePosts and sendPosts
+    const generatePosts = new Function(this, 'generatePosts', {
+      description: "Generates new posts and calls addPost API",
+      code: Code.fromAsset('lib/lambdas/generatePosts/target/x86_64-unknown-linux-musl/release/lambda'),
       runtime: Runtime.PROVIDED_AL2,
       handler: 'not.required',
       timeout: Duration.minutes(5),
       environment: {
         RUST_BACKTRACE: '1',
-        TABLE_NAME: 'Recipes',
-        OPEN_AI_API_KEY: openAiApiKey,
-        BUCKET_NAME: s3Bucket.bucketName,
-        CLOUD_CONVERT_API_KEY: cloudConvertApiKey
+        OPEN_AI_API_KEY: openAiApiKey
       },
       logRetention: RetentionDays.ONE_WEEK,
       role: lambdaRole
     });
 
-    s3Bucket.grantWrite(addRecipeWorker);
-    s3Bucket.addToResourcePolicy(
-      new iam.PolicyStatement({
-        actions: ['s3:PutBucketPolicy'],
-        resources: [s3Bucket.bucketArn],
-        principals: [new iam.ServicePrincipal('lambda.amazonaws.com')],
-      })
-    );
-    // Create an SNS topic and subscribe the addRecipeWorker Lambda function
-    const recipeTopic = new sns.Topic(this, 'AddRecipeTopic');
-    recipeTopic.addSubscription(new LambdaSubscription(addRecipeWorker));
-
-    const addRecipe = new Function(this, 'addRecipe', {
-      description: "Add recipes worker",
-      code: Code.fromAsset('lib/lambdas/addRecipe/target/x86_64-unknown-linux-musl/release/lambda'),
+    const sendPosts = new Function(this, 'sendPosts', {
+      description: "Takes a post to send to SNS topic from DB",
+      code: Code.fromAsset('lib/lambdas/sendPosts/target/x86_64-unknown-linux-musl/release/lambda'),
       runtime: Runtime.PROVIDED_AL2,
       handler: 'not.required',
       timeout: Duration.minutes(5),
       environment: {
         RUST_BACKTRACE: '1',
-        TABLE_NAME: 'Recipes',
-        SNS_ARN: recipeTopic.topicArn
+        TABLE_NAME: 'Posts',
+        SNS_ARN: postTopic.topicArn
       },
       logRetention: RetentionDays.ONE_WEEK,
       role: lambdaRole
     });
 
-    recipeTopic.grantPublish(addRecipe);
+    postTopic.grantPublish(sendPosts);
+    dynamoTable.grantReadWriteData(sendPosts);
+    const generateEvent = new Rule(this, 'generateEvent', {
+      schedule: Schedule.expression('rate(1 week)'),
+    });
+    generateEvent.addTarget(new LambdaFunction(generatePosts));
+    const postEvent = new Rule(this, 'postEvent', {
+      schedule: Schedule.expression('rate(1 hour)'),
+    });
+    postEvent.addTarget(new LambdaFunction(sendPosts));
 
-    // Update a recipe
-    const updateRecipe = new Function(this, 'updateRecipe', {
-      description: "Update recipes",
-      code: Code.fromAsset('lib/lambdas/updateRecipe/target/x86_64-unknown-linux-musl/release/lambda'),
+    // 1 Lambda function API
+    const addPost = new Function(this, 'addPost', {
+      description: "Add new posts to the DB",
+      code: Code.fromAsset('lib/lambdas/addPost/target/x86_64-unknown-linux-musl/release/lambda'),
       runtime: Runtime.PROVIDED_AL2,
       handler: 'not.required',
       timeout: Duration.minutes(5),
       environment: {
         RUST_BACKTRACE: '1',
-        TABLE_NAME: 'Recipes'
+        TABLE_NAME: 'Posts'
       },
       logRetention: RetentionDays.ONE_WEEK,
       role: lambdaRole
     });
 
-    // Add recipe to user's recipe book
-    const collectRecipe = new Function(this, 'collectRecipe', {
-      description: "Add user recipe",
-      code: Code.fromAsset('lib/lambdas/collectRecipe/target/x86_64-unknown-linux-musl/release/lambda'),
-      runtime: Runtime.PROVIDED_AL2,
-      handler: 'not.required',
-      environment: {
-        RUST_BACKTRACE: '1',
-        TABLE_NAME: 'RecipeBooks',
-      },
-      logRetention: RetentionDays.ONE_WEEK
-    });
+    dynamoTable.grantWriteData(addPost);
 
-    // Get a user's recipe book
-    const getUserRecipes = new Function(this, 'getUserRecipes', {
-      description: "Get user's recipe book",
-      code: Code.fromAsset('lib/lambdas/getUserRecipes/target/x86_64-unknown-linux-musl/release/lambda'),
-      runtime: Runtime.PROVIDED_AL2,
-      handler: 'not.required',
-      environment: {
-        RUST_BACKTRACE: '1',
-        USER_TABLE_NAME: 'RecipeBooks',
-        RECIPE_TABLE_NAME: 'Recipes',
-      },
-      logRetention: RetentionDays.ONE_WEEK
-    });
+    // 2 Lambda function subscribers
 
-    userTable.grantFullAccess(collectRecipe);
-    userTable.grantFullAccess(getUserRecipes);
-
-    // Gets all recipes from dynamoDB
-    const getRecipes = new Function(this, 'getRecipes', {
-      description: "Add recipes",
-      code: Code.fromAsset('lib/lambdas/getRecipes/target/x86_64-unknown-linux-musl/release/lambda'),
-      runtime: Runtime.PROVIDED_AL2,
-      handler: 'not.required',
-      environment: {
-        RUST_BACKTRACE: '1',
-        TABLE_NAME: 'Recipes',
-      },
-      logRetention: RetentionDays.ONE_WEEK
-    });
-
-    // Gets all recipes from dynamoDB
-    const tesseract = new Function(this, 'tesseract', {
-      description: "Add recipes",
-      code: Code.fromAsset('lib/lambdas/tesseract/target/x86_64-unknown-linux-musl/release/lambda'),
+    // Post to Twitter
+    const postToTwitter = new Function(this, 'postToTwitter', {
+      description: "posts the post to X",
+      code: Code.fromAsset('lib/lambdas/postToTwitter/target/x86_64-unknown-linux-musl/release/lambda'),
       runtime: Runtime.PROVIDED_AL2,
       handler: 'not.required',
       timeout: Duration.minutes(5),
       environment: {
         RUST_BACKTRACE: '1',
-        OPEN_AI_API_KEY: openAiApiKey,
+        SNS_ARN: postTopic.topicArn,
+        TWITTER_API_KEY: twitterKey,
+        TWITTER_SECRET: twitterSecret
       },
-      logRetention: RetentionDays.ONE_WEEK
+      logRetention: RetentionDays.ONE_WEEK,
+      role: lambdaRole
     });
 
-
-    // Lambda for minting a recipe
-    const mintNFT = new Function(this, 'mintRecipe', {
-      code: lambda.Code.fromAsset(path.join(__dirname, '/lambdas/mintRecipe')),
-      runtime: Runtime.NODEJS_16_X,
-      handler: 'handler.handler',
-      timeout: Duration.minutes(5),
+    // Post to Deso
+    const postToDeso = new Function(this, 'postToDeso', {
+      description: "posts the post to deso",
+      code: Code.fromAsset('lib/lambdas/postToDeso/target/x86_64-unknown-linux-musl/release/lambda'),
+      runtime: Runtime.PROVIDED_AL2,
+      handler: 'not.required',
       environment: {
-        NFT_STORAGE_API_KEY: nftStoreApiKey,
-        PRIVATE_KEY: privateKey,
-      }
+        RUST_BACKTRACE: '1',
+        SNS_ARN: postTopic.topicArn,
+        DESO_USER: desoUser,
+        DESO_PRIVATE_KEY: desoPrivateKey
+      },
+      logRetention: RetentionDays.ONE_WEEK,
+      role: lambdaRole
     });
 
-    // Grant the lambda functions write and read access
-    dynamoTable.grantFullAccess(getRecipes);
-    dynamoTable.grantFullAccess(addRecipe);
-    dynamoTable.grantFullAccess(addRecipeWorker);   
-    dynamoTable.grantReadData(getUserRecipes); 
-    dynamoTable.grantFullAccess(updateRecipe);
+    postTopic.addSubscription(new LambdaSubscription(postToTwitter));
+    postTopic.addSubscription(new LambdaSubscription(postToDeso));
 
 
     // Create an API Gateway resource for each of the CRUD operations
-    const api = new RestApi(this, 'Recipe3API', {
-      restApiName: 'Recipe3 API',
+    const api = new RestApi(this, 'PostAPI', {
+      restApiName: 'Post API',
       defaultCorsPreflightOptions: {
         allowOrigins: Cors.ALL_ORIGINS,
         allowMethods: Cors.ALL_METHODS,
@@ -236,30 +162,10 @@ export class AutoPosterStack extends Stack {
     });
 
     // Integrate lambda functions with an API gateway
-    const mintNFTAPI = new LambdaIntegration(mintNFT);
-    const getRecipesAPI = new LambdaIntegration(getRecipes);
-    const addRecipeAPI = new LambdaIntegration(addRecipe);
-    const collectRecipeAPI = new LambdaIntegration(collectRecipe);
-    const getUserRecipesAPI = new LambdaIntegration(getUserRecipes);
-    const tesseractAPI = new LambdaIntegration(tesseract);
-    const updateRecipeAPI = new LambdaIntegration(updateRecipe);
+    const addPostAPI = new LambdaIntegration(addPost);
 
-    const mint = api.root.addResource('mint');
-    mint.addMethod('POST', mintNFTAPI);
-
-    const books = api.root.addResource('api');
-    books.addMethod('POST', addRecipeAPI);
-    books.addMethod('GET', getRecipesAPI);
-
-    const collect = api.root.addResource('collect');
-    collect.addMethod('POST', collectRecipeAPI);
-    collect.addMethod('GET', getUserRecipesAPI);
-
-    const tess = api.root.addResource('tesseract');
-    tess.addMethod('POST', tesseractAPI);
-
-    const update = api.root.addResource('update');
-    update.addMethod('POST', updateRecipeAPI);
+    const add = api.root.addResource('add');
+    add.addMethod('POST', addPostAPI);
   }
 }
 
---
+++ backend/lib/lambdas/generatePosts/src/main.rs
@@ -0,0 +1,263 @@
+use serde::Deserialize;
+use serde::Serialize;
+use serde_json::{json, Value};
+use std::collections::HashMap;
+use futures_util::future::join_all;
+use reqwest::get;
+use select::document::Document;
+use select::predicate::Name;
+use uuid::Uuid;
+use std::env;
+use openai_api_rs::v1::api;
+use openai_api_rs::v1::chat_completion::{self, ChatCompletionRequest};
+use openai_api_rs::v1::image::ImageGenerationRequest;
+use openai_api_rs::v1::error::APIError;
+use scraper::{Html, Selector};
+use lambda_http::{Response, Body, Error, Request};
+use lambda_runtime::handler_fn;
+use tokio::fs::File;
+use tokio::time::Duration;
+use tokio::fs::File as AsyncFile;
+use tokio_util::codec::{BytesCodec, FramedRead};
+use std::path::Path;
+use std::io::prelude::*;
+use base64;
+use tokio::io::AsyncWriteExt;
+use dotenv::dotenv;
+use std::any::Any;
+use std::str::FromStr;
+use xml::reader::{EventReader, XmlEvent};
+
+
+const PROMPT: &str = "Parse the recipe from the web page content and format it in JSON with the following structure: {name: <str>, ingredients: [], instructions: [], notes: <str>, summary: <str>}. If the words don't have spaces, add spaces so it's readable. Ensure the ingredients and instructions are a list of strings, if they have sections, just add the header as an item in the list.";
+
+#[derive(Debug, Serialize)]
+pub struct SuccessResponse {
+    pub body: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct FailureResponse {
+    pub body: String,
+}
+
+type WorkerResponse = Result<SuccessResponse, FailureResponse>;
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub enum ContentType {
+    POST,
+    THREAD
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Post {
+    pub post: String,
+    pub content_type: ContentType
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Posts {
+    pub posts: Vec<Post>
+}
+
+// Implement Display for the Failure response so that we can then implement Error.
+impl std::fmt::Display for FailureResponse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.body)
+    }
+}
+
+// Implement Error for the FailureResponse so that we can `?` (try) the Response
+// returned by `lambda_runtime::run(func).await` in `fn main`.
+impl std::error::Error for FailureResponse {}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let func = handler_fn(handler);
+    lambda_runtime::run(func).await?;
+
+    Ok(())
+}
+
+async fn get_current_newsletter_content(url: &str) -> Result<SuccessResponse, FailureResponse> {
+    let tag_name = "item";
+    // Send a GET request to the URL
+    let response = match get(url).await {
+        Ok(r) => r,
+        Err(e) => {
+            println!("Error reading URL: {:?} {:?}", url, e);
+            return Err(FailureResponse {
+                body: format!("Error reading URL: {}", e)
+            });
+        }
+    };
+
+    // Read the response body into a string
+    let mut xml_content = match response.text().await {
+        Ok(c) => c,
+        Err(e) => {
+            println!("Error reading URL contents: {:?}", e);
+            return Err(FailureResponse {
+                body: format!("Error reading URL contents: {}", e)
+            });
+        }
+    };
+
+    // Parse XML content
+    let parser = EventReader::new(xml_content.as_bytes());
+    let mut inside_tag = false;
+    let mut result = String::new();
+    
+    for event in parser {
+        match event {
+            Ok(XmlEvent::StartElement { name, .. }) if name.local_name == tag_name => {
+                inside_tag = true;
+            }
+            Ok(XmlEvent::EndElement { name }) if name.local_name == tag_name => {
+                break;
+            }
+            Ok(XmlEvent::CData(text)) if inside_tag => {
+                result.push_str(&text);
+            }
+            _ => {}
+        }
+    }
+    
+    return Ok(SuccessResponse {
+        body: result
+    });
+}
+
+async fn get_api_key() -> Option<String> {
+    env::var("OPEN_AI_API_KEY").ok()
+}
+
+async fn generate_posts(contents: String) -> Result<Posts, FailureResponse> {
+    let open_ai_api_key = get_api_key().await;
+    if let Some(api_key) = open_ai_api_key {
+        let client = api::Client::new(api_key);
+        let req = ChatCompletionRequest {
+            model: chat_completion::GPT4.to_string(),
+            messages: vec![chat_completion::ChatCompletionMessage {
+                role: chat_completion::MessageRole::user,
+                content: format!("{} {}", PROMPT.to_string(), contents),
+                name: None,
+                function_call: None,
+            }],
+            functions: None,
+            function_call: None,
+            temperature: None,
+            top_p: None,
+            n: None,
+            stream: None,
+            stop: None,
+            max_tokens: None,
+            presence_penalty: None,
+            frequency_penalty: None,
+            logit_bias: None,
+            user: None,
+        };
+        println!("Chat Request: {:?}", req);
+        let result = match client.chat_completion(req).await {
+            Ok(r) => r,
+            Err(e) => {
+                println!("Error with OpenAI: {:?}", e);
+                return Err(FailureResponse {
+                    body: format!("Error getting response from OpenAI: {:?}", e)
+                });
+            }
+        };
+        println!("{:?}", result.choices[0].message.content);
+        let generated_content = match &result.choices[0].message.content {
+            Some(c) => c,
+            None => {
+                println!("Could not get message content");
+                return Err(FailureResponse {
+                    body: format!("Could not get message content")
+                })
+            },
+        };
+        let content = match extract_json(&generated_content) {
+            Some(s) => s,
+            None => {
+                println!("Error parsing recipe conents!");
+                return Err(FailureResponse {
+                    body: format!("Error parsing recipe contents!")
+                });            
+            },
+        };
+        let posts: Posts = match serde_json::from_str(&content) {
+            Ok(r) => r,
+            Err(e) => {
+                println!("Error parsing JSON {:?}", e);
+                return Err(FailureResponse {
+                    body: format!("Error parsing JSON {:?}", e)
+                });
+            }
+        };
+        return Ok(posts);
+    }
+    return Err(FailureResponse {
+        body: String::from("API Key Not Set")
+    });
+}
+
+
+
+async fn generate_uuid() -> String {
+    Uuid::new_v4().to_string()
+}
+
+
+/**
+ * Calls our add to db API
+ */
+pub async fn add_to_db(posts: Posts) -> Result<String, Error> {
+    Ok(String::from("Ok"))
+}
+
+fn extract_json(json_string: &str) -> Option<String> {
+    // Find the positions of the first opening and closing curly braces
+    let start_pos = json_string.find('{');
+    let end_pos = json_string.find('}');
+
+    if let (Some(start), Some(end)) = (start_pos, end_pos) {
+        // Extract the content between the curly braces, including the braces themselves
+        let json_body = &json_string[start..=end];
+        return Some(json_body.trim().to_string());
+    }
+
+    // If no match was found, return None
+    None
+}
+
+
+async fn handler(_event: Value, _ctx: lambda_runtime::Context) -> Result<String, Error> {
+    // 1. First retrieve the current contents of our newsletters
+
+    // 2. Generate content
+
+    // 3. Upload to the DB
+
+    Ok("Success!".to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! aw {
+        ($e:expr) => {
+            tokio_test::block_on($e)
+        };
+    }
+
+    #[test]
+    fn get_newsletter_content() {
+        let url = "https://davidjmeyer.substack.com/feed";
+
+        let response = aw!(get_current_newsletter_content(url));
+        println!("Response: {:?}", response);
+    }
+
+}
